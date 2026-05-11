942283e9a31a168c2660ab376e390b40e7315414
SolarOracle <antoniopsmasetti@gmail.com>
2026-05-11 16:08:10 -0300

    Música Provisória Adicionada.

---
+++ Assets/BallHorse/Script/MusicPlayerTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MusicPlayerTest : MonoBehaviour
+{
+    public static MusicPlayerTest instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+
