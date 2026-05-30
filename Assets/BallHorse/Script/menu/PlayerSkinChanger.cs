a11b0e3b9a98d6f65d5c9578f3a9f85b2c89acdf
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2026-05-30 12:22:29 -0300

    Animção cavalo voando
    ta funcionando bem mais ou menos, tem um bug fdp que eu nn tenho ideia de como resolver, quero mostrar pro Reinaldo pra pedir ajuda pra ele

--- Assets/BallHorse/Script/menu/PlayerSkinChanger.cs
+++ Assets/BallHorse/Script/menu/PlayerSkinChanger.cs
@@ -5,6 +5,7 @@ public class PlayerSkinChanger : MonoBehaviour
     public Animator anim;
     public static string skin;
     public Pause pauseScript;
+    public Player playerScript;
 
    private void Awake()
     {
@@ -14,26 +15,66 @@ public class PlayerSkinChanger : MonoBehaviour
         {
             anim.Play("UniClip");
             skin = "unicorn";
+            if (playerScript.powerup == "None")
+            {
+                anim.SetBool("FlightUni", false);
+            }
+            else if (playerScript.powerup == "Flight")
+            {
+                anim.SetBool("FlightUni", true);
+            }
         }
-        if (SkinSprite.isUni == false && SkinSprite.isAlien == true && SkinSprite.isPadrao == false && SkinSprite.isChiclete == false && SkinSprite.isReal == false)
+        else if (SkinSprite.isUni == false && SkinSprite.isAlien == true && SkinSprite.isPadrao == false && SkinSprite.isChiclete == false && SkinSprite.isReal == false)
         {
             anim.Play("AlienClip");
             skin = "alien";
+            if (playerScript.powerup == "None")
+            {
+                anim.SetBool("FlightAlien", false);
+            }
+            else if (playerScript.powerup == "Flight")
+            {
+                anim.SetBool("FlightAlien", true);
+            }
         }
-        if (SkinSprite.isUni == false && SkinSprite.isAlien == false && SkinSprite.isPadrao == true && SkinSprite.isChiclete == false && SkinSprite.isReal == false)
+        else if (SkinSprite.isUni == false && SkinSprite.isAlien == false && SkinSprite.isPadrao == true && SkinSprite.isChiclete == false && SkinSprite.isReal == false)
         {
             anim.Play("CavaloBolaClip");
             skin = "ballhorse";
+            if(playerScript.powerup == "None")
+            {
+                anim.SetBool("FlightBola", false);
+            }
+            else if(playerScript.powerup == "Flight")
+            {
+                anim.SetBool("FlightBola", true);
+            }
         }
-        if (SkinSprite.isUni == false && SkinSprite.isAlien == false && SkinSprite.isPadrao == false && SkinSprite.isChiclete == true && SkinSprite.isReal == false)
+        else if (SkinSprite.isUni == false && SkinSprite.isAlien == false && SkinSprite.isPadrao == false && SkinSprite.isChiclete == true && SkinSprite.isReal == false)
         {
             anim.Play("ChicleteClip");
             skin = "bubblegum";
+            if (playerScript.powerup == "None")
+            {
+                anim.SetBool("FlightChic", false);
+            }
+            else if (playerScript.powerup == "Flight")
+            {
+                anim.SetBool("FlightChic", true);
+            }
         }
-        if (SkinSprite.isUni == false && SkinSprite.isAlien == false && SkinSprite.isPadrao == false && SkinSprite.isChiclete == false && SkinSprite.isReal == true)
+        else if (SkinSprite.isUni == false && SkinSprite.isAlien == false && SkinSprite.isPadrao == false && SkinSprite.isChiclete == false && SkinSprite.isReal == true)
         {
             anim.Play("RealClip");
             skin = "real";
+            if (playerScript.powerup == "None")
+            {
+                anim.SetBool("FlightReal", false);
+            }
+            else if (playerScript.powerup == "Flight")
+            {
+                anim.SetBool("FlightReal", true);
+            }
         }
 
     }
