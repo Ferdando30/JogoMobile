b81907293bcc2494b1cadd4a4a40284de0191629
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2026-06-17 08:18:28 -0300

    Bug Fix
    concertei o bug das skins não serem mais selecionaveis depois que começa uma partida e loopei a musica

---
+++ Assets/BallHorse/Script/menu/SelectBtnBugFix.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectBtnBugFix : MonoBehaviour
+{
+    SkinSprite skinSpriteScript;
+    public Button SelectButton;
+    public string qualSkin;
+
+    private void Awake()
+    {
+        skinSpriteScript = GameObject.FindGameObjectWithTag("Don'tDestroyManager").GetComponent<SkinSprite>();
+        if(qualSkin == "Unicornio")
+        {
+            SelectButton.onClick.AddListener(skinSpriteScript.SelectUni);
+        }
+
+        else if(qualSkin == "Padrao")
+        {
+            SelectButton.onClick.AddListener(skinSpriteScript.SelectPadrao);
+        }
+
+        else if (qualSkin == "Chiclete")
+        {
+            SelectButton.onClick.AddListener(skinSpriteScript.SelectChic);
+        }
+
+        else if (qualSkin == "Real")
+        {
+            SelectButton.onClick.AddListener(skinSpriteScript.SelectReal);
+        }
+
+        else if (qualSkin == "Alien")
+        {
+            SelectButton.onClick.AddListener(skinSpriteScript.SelectAlien);
+        }
+    }
+}
