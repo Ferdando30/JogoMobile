18eac7b0a978dcd11d9d660c6a770497b9b78d1a
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2026-06-28 15:16:29 -0300

    Pause Update
    coloqui o music slider no pause do jogo

--- Assets/BallHorse/Script/menu/MusicSlider.cs
+++ Assets/BallHorse/Script/menu/MusicSlider.cs
@@ -8,16 +8,52 @@ public class MusicSlider : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
+    private const string MusicVolumeKey = "MusicVolume";
+    private const string SfxVolumeKey = "SFXVolume";
+
+    private const string MusicMixerParameter = "MyExposedMusic";
+    private const string SfxMixerParameter = "SFXexposed";
+
+    private void Start()
+    {
+        LoadVolumes();
+    }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
-        myMixer.SetFloat("MyExposedMusic", Mathf.Log10(volume)*20);
+
+        SetMixerVolume(MusicMixerParameter, volume);
+
+        PlayerPrefs.SetFloat(MusicVolumeKey, volume);
+        PlayerPrefs.Save();
     }
 
     public void SetSFXVolume()
     {
         float volume = sfxSlider.value;
-        myMixer.SetFloat("SFXexposed", Mathf.Log10(volume) *20);
+
+        SetMixerVolume(SfxMixerParameter, volume);
+
+        PlayerPrefs.SetFloat(SfxVolumeKey, volume);
+        PlayerPrefs.Save();
+    }
+   
+    private void LoadVolumes()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MusicVolumeKey, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SfxVolumeKey, 1f);
+
+        musicSlider.value = musicVolume;
+        sfxSlider.value = sfxVolume;
+
+        SetMixerVolume(MusicMixerParameter, musicVolume);
+        SetMixerVolume(SfxMixerParameter, sfxVolume);
+    }
+
+    private void SetMixerVolume(string parameterName, float volume)
+    {
+        volume = Mathf.Clamp(volume, 0.0001f, 1f);
+        myMixer.SetFloat(parameterName, Mathf.Log10(volume) * 20);
     }
 }
--- Assets/BallHorse/Script/menu/Pause.cs
+++ Assets/BallHorse/Script/menu/Pause.cs
@@ -12,11 +12,19 @@ public class Pause : MonoBehaviour
     public BetterSpawner spawner;
     public Player playerScript;
     public GameObject PausedImg;
+    public Slider musicSlider;
+    public Button Replay;
+    public Button OptionsBtn;
+    public Button Home;
+    public Button BackBtn;
+
     void Awake()
     {
         isPaused = false;
         MoedaAnim = MoeadaObjt.GetComponent<Animator>();
         PausedImg.SetActive(false);
+        musicSlider.gameObject.SetActive(false);
+        BackBtn.gameObject.SetActive(false);
     }
 
     public void Pausando()
@@ -35,7 +43,28 @@ public class Pause : MonoBehaviour
         MoedaAnim.speed = 1f;
         spawner.ActivateObjects();
         spawner.active = true;
-        playerScript.rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        playerScript.rb.linearVelocity = Vector2.zero;
+        playerScript.rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
         PausedImg.SetActive(false);
+        musicSlider.gameObject.SetActive(false);
+        BackBtn.gameObject.SetActive(false);
+    }
+
+    public void Options()
+    {
+        musicSlider.gameObject.SetActive(true);
+        Replay.gameObject.SetActive(false);
+        OptionsBtn.gameObject.SetActive(false);
+        Home.gameObject.SetActive(false);
+        BackBtn.gameObject.SetActive(true);
+    }
+    
+    public void Back()
+    {
+        musicSlider.gameObject.SetActive(false);
+        Replay.gameObject.SetActive(true);
+        OptionsBtn.gameObject.SetActive(true);
+        Home.gameObject.SetActive(true);
+        BackBtn.gameObject.SetActive(false);
     }
 }
